2072e4f34488bfe35e7b370ef0a1be4f09b6d5b5
4193883-eng <83756034+4193883-eng@users.noreply.github.com>
2023-09-27 19:35:43 +0300

    Add animations

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,71 @@
-import {Button, ListItem, UnorderedList} from "@chakra-ui/react";
+import { Button, ListItem, UnorderedList } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+const MotionListItem = motion(ListItem)
+const MotionButton = motion(Button)
 
 export function ContactList({ contacts, filter, removeHandler }) {
-	function search(contacts){
-		if (filter == ""){return contacts}
-		return contacts.map((contact) => {
-			const lowerName = contact.name.toLowerCase()
-			const lowerQuery = filter.toLowerCase()
-			if(lowerName.includes(lowerQuery)){
-				return contact
-			}
-		})
-	}
+    function search(contacts) {
+        if (filter == '') {
+            return contacts
+        }
+        return contacts.map((contact) => {
+            const lowerName = contact.name.toLowerCase()
+            const lowerQuery = filter.toLowerCase()
+            if (lowerName.includes(lowerQuery)) {
+                return contact
+            }
+        })
+    }
 
-	return (
-		<UnorderedList>
-			{search(contacts).map((contact) =>
-			{if(contact) { return <ListItem h={"fit-content"} key={contact.id}>{contact.name}: +380-{contact.number} <Button h={"90%"} onClick={() => {removeHandler(contact.id); console.log(contact.id)}} >Remove</Button></ListItem>}})
-			}
-		</UnorderedList>
-	)
+    return (
+        <UnorderedList>
+            <AnimatePresence>
+                {search(contacts).map((contact) => {
+                    if (contact) {
+                        return (
+                            <MotionListItem
+                                h={'fit-content'}
+                                key={contact.id}
+                                initial={{
+                                    opacity: 0,
+                                    translateX: '100%',
+                                    animation: 'ease-out',
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    translateX: 0,
+                                    animation: 'ease-out',
+                                }}
+                                exit={{
+                                    opacity: 0,
+                                    translateX: '-100%',
+                                    animation: 'ease-out',
+                                }}
+                                transition={{
+                                    duration: 0.5,
+                                    animation: 'ease-out',
+                                }}
+                            >
+                                {contact.name}: +380-{contact.number}
+                                <MotionButton
+                                    whileHover={{ scale: 1.05 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    h={'90%'}
+                                    m={5}
+                                    onClick={() => {
+                                        setTimeout(() => {
+                                            removeHandler(contact.id)
+                                        }, 100)
+                                    }}
+                                >
+                                    Remove
+                                </MotionButton>
+                            </MotionListItem>
+                        )
+                    }
+                })}
+            </AnimatePresence>
+        </UnorderedList>
+    )
 }
